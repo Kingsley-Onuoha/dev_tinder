57c9a89e5224e46fb3fa784100095dc117d65ba7
Kingsley Onuoha <onuohakingsley521@gmail.com>
2024-10-05 05:37:01 +0100

    routing basics

--- src/app.js
+++ src/app.js
@@ -2,8 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
-  res.send("Hello from the server!");
+app.get("/user", (req, res) => {
+  res.send({
+    firstName: "Kingsley",
+    lastName: "Onuoha",
+  });
+});
+app.post("/user", (req, res) => {
+  res.send("Data successfully sent to DB");
 });
 
 app.listen(3000, () => {
